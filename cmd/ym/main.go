04424fcb68e0e22159d396b7d3223b9c3ffce001
Eli Mallon <eli.mallon@csats.com>
2015-12-24 01:43:41 +0000

    Convert from YAML to JSON and back.

--- cmd/ym/main.go
+++ cmd/ym/main.go
@@ -3,16 +3,27 @@ package main
 import (
     "fmt"
     "os"
-    "log"
     "io/ioutil"
     "github.com/ghodss/yaml"
+    "encoding/json"
 )
 
 func main() {
   byteArray, _ := ioutil.ReadAll(os.Stdin)
-  y, err := yaml.YAMLToJSON(byteArray)
+  var o interface{}
+  var y []byte
+
+  err := json.Unmarshal(byteArray, &o)
+  if err == nil {
+    // JSON decoding succeeded, it's JSON
+    y, err = yaml.JSONToYAML(byteArray)
+  } else {
+    // JSON decoding failed, it's probably YAML
+    y, err = yaml.YAMLToJSON(byteArray)
+  }
+
   if err != nil {
-    log.Fatalf("error: %v", err)
+    fmt.Fprintf(os.Stderr, "ym: %v", err)
   }
   fmt.Println(string(y))
 }
